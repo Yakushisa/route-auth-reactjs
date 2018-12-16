6b83e5843d084d8ba32da3c569470ae02fffd29c
Yakushisa <44353320+Yakushisa@users.noreply.github.com>
2018-12-16 21:50:41 -0200

    Building Project

---
+++ build/precache-manifest.2d8d4cdf8e0f362f8d546c61dce380b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da4113dc9ad2da76c2b0",
+    "url": "/static/js/main.da4113dc.chunk.js"
+  },
+  {
+    "revision": "4fca9d28de3c0a1b2c7e",
+    "url": "/static/js/1.4fca9d28.chunk.js"
+  },
+  {
+    "revision": "61cccc2344fbec584cb8a0a71680e0d0",
+    "url": "/index.html"
+  }
+];
